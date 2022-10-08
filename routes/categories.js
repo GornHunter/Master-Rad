14417a61048501425524a011ccb07567fd6239b5
Igor Karadzic <igigotika@gmail.com>
2022-10-08 15:29:29 +0200

    izbrisani category js fajlovi jer su nepotrebni

--- routes/categories.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const {getAllCategories, createCategory, getCategory, updateCategory, deleteCategory} = require('../controllers/categories')
-
-router.route('/').get(getAllCategories).post(createCategory)
-router.route('/:id').get(getCategory).patch(updateCategory).delete(deleteCategory)
-
-module.exports = router
